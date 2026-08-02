527d95bb0ecf63571d9c1512129b1b6e27a2a199
Mukul <mukulyadav2302@gmail.com>
2026-08-02 21:20:43 +0530

    connected to MongoDB Atlas via mongoose

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
+const connectDB = require("./src/config/db.js");
 const app = require("./app.js");
 
 const PORT = process.env.PORT || 3001;
+const startServer = async () => {
+  await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
